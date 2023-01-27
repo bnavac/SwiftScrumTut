f2d357d3b4beae8c141773c92966fe52817cb631
lForger <70664095+lForger@users.noreply.github.com>
2023-01-26 19:26:24 -0500

    Begun tutorial
    Created the project in xcode

---
+++ SwiftScrum/SwiftScrum/SwiftScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScrumApp.swift
+//  SwiftScrum
+//
+//  Created by Nicholas Bilsborrow on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
